0f7c8556826695f1716285c885d3daa16e9d427f
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2022-11-26 12:05:16 +0100

    API post

--- back/app.js
+++ back/app.js
@@ -1,25 +1,23 @@
+const cors = require('cors');
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const app = express();
 
-// Body Parser config 
-app.use(bodyParser.urlencoded({ extended: true }));  //force le parse dans les objet imbriqué
-app.use(bodyParser.json());
-// const helmet = require("helmet");
+// app.use(bodyParser.urlencoded({ extended: true }));  //force le parse dans les objet imbriqué
+// app.use(bodyParser.json());
 
-// //rajoute entete a l'objet response qu'on renvoi au navigateur - CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    console.log("header set");
-    next();
-});
+// CORS
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500/index.html');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     console.log("header set");
+//     next();
+// });
+app.use(cors());
 
 
 // app.use(helmet());
-
-// app.use('/images', express.static('resources'));
 
 app.get('/gettest', (req, res) => {
   console.log("req body : ", req.body);
@@ -29,20 +27,17 @@ app.get('/gettest', (req, res) => {
 })
 
 
-
 app.post('/posttest', (req, res) => {
   console.log('GET request received', req.body);
   // res.header("Access-Control-Allow-Headers","*");
   // res.header("Access-Control-Allow-Origin", "*");
   // res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
-  // res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  // res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept,\
+  // X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
-  res.send('Hello World!')
+  res.json({ message: 'Hello World!' });
+  // res.send('Hello World!')
+  // res.post('hello');
 })
-
-// //Route use
-// app.use('/api/users', userRoutes);
-// app.use('/api/posts', postRoutes);  
-
 
 module.exports = app;
